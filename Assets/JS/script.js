57677eb0466a2098fce10d600b2190314fd324b1
An-109 <doggod232471@gmail.com>
2024-09-07 22:12:36 -0600

    commit

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,21 +1,63 @@
 const timerButton = document.querySelector(`.button`)
 const timerBox = document.getElementById(`countDownBox`)
 const timerButton2 = document.getElementById(`timerButton`)
+const timerButton3 = document.querySelector(`.resetButton`)
 let timeSet = 0;
+let timerRunning = false;
+let time;
 
-timerButton2.addEventListener(`click`, function(event) {
+timerButton.addEventListener(`click`, function(event) {
     event.preventDefault();
 
+    if (!timerRunning) {
+        timerRunning = true;
+        time = setInterval(() => {
+            let hours = Math.floor(timeSet / 3600);
+            let minutes = Math.floor((timeSet % 3600) / 60);
+            let seconds = timeSet % 60;
+            let timeFormat =
+                String(hours).padStart(2, `0`) + ":"+
+                String(minutes).padStart(2, `0`) + ":"+
+                String(seconds).padStart(2, '0');
 
-    let currentTime = timeSet;
-    const time = setInterval(() => {
-        timerBox.textContent = currentTime;
-        currentTime++;
+            timerBox.textContent = timeFormat;
+            timeSet++;
+        }, 1000);
+    } else {
+        timerRunning = false;
+        clearInterval(time);
+    }
 
-        if (currentTime === 10) {
-            clearInterval(time);
-            timerBox.textContent = 'Time’s up!';
-        }
-    }, 1000);
+    
 
-})
+});
+
+timerButton2.addEventListener(`click`, function(event){
+    event.preventDefault();
+    if (timerRunning) {
+        timerRunning = false;
+        clearInterval(time);
+    }
+
+});
+
+timerButton3.addEventListener(`click`, function(event){
+    event.preventDefault();
+    if (timerRunning) {
+        clearInterval(time);
+        timerRunning = false;
+    }
+        timeSet = 0;
+        
+        let hours = Math.floor(timeSet / 3600);
+            let minutes = Math.floor((timeSet % 3600) / 60);
+            let seconds = timeSet % 60;
+            let timeFormat =
+                String(hours).padStart(2, `0`) + ":"+
+                String(minutes).padStart(2, `0`) + ":"+
+                String(seconds).padStart(2, '0');
+
+            timerBox.textContent = timeFormat;
+    
+
+});
